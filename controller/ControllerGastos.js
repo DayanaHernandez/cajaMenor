f1a06d5464fab6d6802b204bb145fe7054503a20
DayanaHernandez <dayanahernandezhincapie@gmail.com>
2023-11-16 22:55:11 -0500

    se actualiza con codigo del profesor

---
+++ controller/ControllerGastos.js
@@ -0,0 +1,96 @@
+import { gastos } from "../model/ModelGastos.js";
+console.log(gastos);
+
+let btnIniciar = document.getElementById("btnIniciar");
+btnIniciar.addEventListener("click", () => {
+  console.log("iniciando sesión");
+  let usuario = document.getElementById("usuario").value;
+  let contrasena = document.getElementById("usuario").value;
+  let correo = document.getElementById("usuario").value;
+  console.log(usuario, contrasena, correo);
+  // if (usuario == "Dayana") {
+  //   if (contrasena == "dayana56") {
+  //     if (correo == "dayanahernandezhincapie@gmail.com") {
+  //       console.log("Inicio de sesion correcta");
+  document.getElementById("spinner").style.display = "block";
+  setTimeout(() => {
+    document.getElementById("spinner").style.display = "none";
+    document.getElementById("login").style.display = "none";
+    document.getElementById("panel").style.display = "block";
+  }, 3000);
+  //     } else {
+  //       console.log("Datos incorrectos");
+  //     }
+  //   } else {
+  //     console.log("Datos incorrectos");
+  //   }
+  // } else {
+  //   console.log("Datos incorrectos");
+  // }
+});
+
+let cerrarSesion = document.getElementById("cerrarSesion");
+cerrarSesion.addEventListener("click", () => {
+  document.getElementById("login").style.display = "block";
+  document.getElementById("panel").style.display = "none";
+});
+// Tope Maximo
+var btnAsignar = document.getElementById("btnAsignar");
+btnAsignar.addEventListener("click", () => {
+  let prueba = document.getElementById("topeMaximoInput").value;
+  document.getElementById("topeMaximo").innerText = "Tope Maximo: " + prueba;
+});
+
+let listar = document.getElementById('listar')
+listar.addEventListener('click', function () {
+    document.getElementById('sectionListar').innerHTML = ''//formateador de codigo 
+    document.getElementById('sectionListar').style.zIndex = '3'
+    document.getElementById('sectionBuscar').style.zIndex = '2'
+    document.getElementById('sectionRegistro').style.zIndex = '2'
+    //Siempre se debe de trabajar con una funcion dentro del metodo
+    gastos.map((gasto) => {
+        let categoria = document.createElement('p')
+        let descripcion = document.createElement('p')
+        let valor = document.createElement('p')
+        let card = document.createElement('article')
+        categoria.textContent = gasto.categoria
+        descripcion.textContent = gasto.descripcion
+        valor.textContent = gasto.valor
+        card.classList.add('card')
+        card.append(categoria, descripcion, valor)
+        document.getElementById('sectionListar').append(card)
+    })
+})
+
+let registrar = document.getElementById('registrar')
+// if(prueba > 0 && btnIniciar )
+registrar.addEventListener('click', function () {
+    document.getElementById('sectionListar').style.zIndex = '2'
+    document.getElementById('sectionBuscar').style.zIndex = '2'
+    document.getElementById('sectionRegistro').style.zIndex = '3'
+})
+let btnGuardar = document.getElementById('btnGuardar')
+btnGuardar.addEventListener('click', () => {
+    let fcategoria = document.getElementById('fcategoria').value
+    let fdescripcion = document.getElementById('fdescripcion').value
+    let fvalor = document.getElementById('fvalor').value
+    let gasto = {
+        id: Math.random() * 10,
+        categoria: fcategoria,
+        descripcion: fdescripcion,
+        valor: fvalor
+    }
+    gastos.push(gasto)
+    
+})
+
+let buscar = document.getElementById('buscar')
+buscar.addEventListener('click', function () {
+    document.getElementById('sectionListar').style.zIndex = '2'
+    document.getElementById('sectionBuscar').style.zIndex = '3'
+    document.getElementById('sectionRegistro').style.zIndex = '2'
+})
+
+
+
+
---
+++ model/ModelGastos.js
@@ -0,0 +1,8 @@
+export let gastos = [
+  {
+    id: 23,
+    categoria: 'Cualquier cosa',
+    descripcion: 'Cualquier cosa',
+    valor: 5000
+}
+];
